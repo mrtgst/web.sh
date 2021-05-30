d27a2ccdbd67d125ef04d7c4b9c4e791261fd6de
mrtgst <johnmartingustavsson@gmail.com>
2021-05-30 10:07:20 +0200

    add style.css and header, navbar, footer

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 # builds the website by importing from the content folder
 
+CURRENT_YEAR=$(date +"%Y")
 init () {
 	if ! [ -d ./content ]; then mkdir ./content; fi
 	printf "# these variables contain the site metadata
@@ -17,22 +18,68 @@ source_metadata () {
 
 
 add_figlet () {
-	echo "<pre>" >> $1
+	printf "<pre>\n" >> $1
 	figlet -f smkeyboard -w 1080 "${FIGLET_TITLE_TEXT}" >> $1
-	echo '</pre>' >> $1
+	printf '</pre>\n' >> $1
 }
 
 
+add_navbar () {
+printf "\
+<div class="navbar">
+<ul>
+	<li display="inline"><a href="blog.html">blog</a></li>
+	<li display="inline"><a href="about.html">about</a></li>
+</ul>
+</div>\n" >> $1 
+}
+
+add_footer () {
+printf '
+<footer>
+	&copy; %s<br>No scripts, no cookies.
+</footer>\n' ${CURRENT_YEAR}\
+>> $1
+}
 
 make_index () {
-echo "\
-<html>
+printf "\
+<!doctype html>
+<html lang="en">
 <head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width,initial-scale=1">
+<link rel="stylesheet" type="text/css" href="style.css">
 <title>${TITLE}</title>
 </head>
-<body>" > index.html
-add_figlet 'index.html'
-echo "\
+<body>
+<header>\n"\
+> index.html
+
+add_figlet index.html
+
+printf "\
+</header>\n"\
+>> index.html
+
+add_navbar index.html
+
+printf '
+<div class="row">
+<div class="column side">
+<!-- left-hand column -->
+</div>
+<div class="column middle">
+</div>
+<div class="column side">
+<!-- right-hand column -->
+</div>
+</div> <!-- end row div -->\n'\
+>> index.html
+
+add_footer index.html
+
+printf "\
 </body>
 </html>\
 " >> index.html
