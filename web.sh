e785e062fb35ea19780a6592bfb6b4f8de0f4011
mrtgst <johnmartingustavsson@gmail.com>
2021-06-06 16:03:17 +0200

    refactor and put init in build function

--- web.sh
+++ web.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-VERSION=0.1.28
-TARGET_DIR=$2
+VERSION=0.1.29
+ROOT=$2
+CONTENT_DIR=${ROOT}/content
+BLOG_DIR=${ROOT}/blog
 CURRENT_YEAR=$(date +"%Y")
 CURRENT_DATE=$(date +"%Y-%m-%d")
 
@@ -12,37 +14,32 @@ get_size () {
 
 init () {
 	# initiate template files
-	if ! [ -d ${TARGET_DIR}/content ]; then mkdir -p ${TARGET_DIR}/content; fi
-	if ! [ -d ${TARGET_DIR}/blog ]; then 
-		mkdir -p ${TARGET_DIR}/blog  
-    	printf "Some blog text." > ${TARGET_DIR}/blog/${CURRENT_DATE}_Blog-Title.md
+	if ! [ -d ${CONTENT_DIR} ]; then mkdir -p ${CONTENT_DIR}; fi
+	if ! [ -d ${BLOG_DIR} ]; then 
+		mkdir -p ${BLOG_DIR}  
+    	printf "Some blog text." > ${BLOG_DIR}/${CURRENT_DATE}_Blog-Title.md
 	fi
-	if ! [ -e ${TARGET_DIR}/content/blank_text ]; then echo "" > ${TARGET_DIR}/content/blank_text; fi
-	if ! [ -e ${TARGET_DIR}/content/home_text ]; then echo "Welcome to ${0}" > ${TARGET_DIR}/content/home_text; fi
-	if ! [ -e ${TARGET_DIR}/content/about_text ]; then echo "Something about ${0}" > ${TARGET_DIR}/content/about_text; fi
+	if ! [ -e ${CONTENT_DIR}/home_text ]; then echo "Welcome to ${0}" > ${CONTENT_DIR}/home_text; fi
+	if ! [ -e ${CONTENT_DIR}/about_text ]; then echo "Something about ${0}" > ${CONTENT_DIR}/about_text; fi
 
 	# write to metadata file
-	if ! [ -e ${TARGET_DIR}/content/metadata ]; then
+	if ! [ -e ${CONTENT_DIR}/metadata ]; then
 		printf "# these variables contain the site metadata
 		# edit them to fit your site\n
 		TITLE=%s
 		FIGLET_TITLE_TEXT=%s
-		# available figlet fonts: 
-		# banner, block, digital, lean, mnemonic, shadow, small, smshadow, standard,
-		# big, bubble, ivrit, mini, script, slant, smscript, smslant, term
 		FIGLET_FONT='small'
-		" ${0} ${0} > ${TARGET_DIR}/content/metadata
-		echo "Wrote ${TARGET_DIR}/content/metadata file"
+		" ${0} ${0} > ${CONTENT_DIR}/metadata
+		echo "Wrote ${CONTENT_DIR}/metadata file"
 	fi
 
 	# copy stylesheet
-	cp ./style.css ${TARGET_DIR} &&\
-	echo "Copied stylesheet to ${TARGET_DIR}/style.css"
+	cp ./style.css ${ROOT} 
 }
 
 source_metadata () {
 	# source metadata file
-	. ${TARGET_DIR}/content/metadata
+	. ${CONTENT_DIR}/metadata
 }
 
 
@@ -117,79 +114,78 @@ add_header () {
 
 
 build_page () {
-TARGET_FILE=${1}
-SOURCE_FILE=${2}
-BUILD_TARGET=${TARGET_DIR}/${TARGET_FILE}
-BUILD_SOURCE=${TARGET_DIR}/content/${SOURCE_FILE}
-
-add_header ${BUILD_TARGET}
-
-printf '
-<div class="column middle">
-<div class="row">'\
->> "${BUILD_TARGET}" 
-
-if [ -e ${BUILD_SOURCE} ]; then
-	cat "${BUILD_SOURCE}" >> "${BUILD_TARGET}"
-fi
-
-printf '
-</div>
-</div>\n'\
->> "${BUILD_TARGET}"
-
-add_footer "${BUILD_TARGET}"
-
-printf "\
-</body>
-</html>\
-" >> "${BUILD_TARGET}"
+	TARGET_FILE=${1}
+	SOURCE_FILE=${2}
+	BUILD_TARGET=${ROOT}/${TARGET_FILE}
+	BUILD_SOURCE=${CONTENT_DIR}/${SOURCE_FILE}
+	
+	add_header ${BUILD_TARGET}
+	
+	printf '
+	<div class="column middle">
+	<div class="row">'\
+	>> "${BUILD_TARGET}" 
+	
+	if [ -e ${BUILD_SOURCE} ]; then
+		cat "${BUILD_SOURCE}" >> "${BUILD_TARGET}"
+	fi
+	
+	printf '
+	</div>
+	</div>\n'\
+	>> "${BUILD_TARGET}"
+	
+	add_footer "${BUILD_TARGET}"
+	
+	printf "\
+	</body>
+	</html>\
+	" >> "${BUILD_TARGET}"
 
 }
 
 build_blog_post () {
-local posts=$(ls $1)
-LENGTH=${#BLOG_DIR} 
-LENGTH=$(( LENGTH + 1 )) 
-for i in ${posts}; do
-	path=${i%.*} # remove .md
-	tmpfile=$path.tmp
-	title=$(echo "$path" | cut -c ${LENGTH}-) # keep only title
-	printf 'Blog post \"%s\" ' "$title"
-	titlef=$(echo $title | cut -d $'_' -f2 | sed 's/-/\ /g') # replace dashes with space
-	datef=$(echo $title | cut -d $'_' -f1 ) # replace dashes with space
-	datef=$(date -d $datef +'%B %d, %Y')
-	printf '<p><small>%s</small></p>\n' "$datef" > $tmpfile
-	cat $i >> $tmpfile 
-	sed -i "2s/^/## $titlef\n/" $tmpfile # add title on first line
-	# add dinkus and posted date
-	printf '<br><br><p><center><small><pre>* * *</pre></small></center></p>\n' "$datef" >> $tmpfile
-	pandoc $tmpfile --from markdown --to html --output $tmpfile.html
-	build_blog_page $path.html $tmpfile.html '../'
-	rm -f $tmpfile $tmpfile.html
-	printf '... Done\n'
-done
+	local posts=$(ls $1)
+	local length=${#BLOG_DIR} 
+	length=$(( length + 2 )) 
+	for i in ${posts}; do
+		path=${i%.*} # remove .md
+		tmpfile=$path.tmp
+		title=$(echo "$path" | cut -c ${length}-) # keep only title
+		printf 'Building blog post \"%s\" ' "$title"
+		titlef=$(echo $title | cut -d $'_' -f2 | sed 's/-/\ /g') # replace dashes with space
+		datef=$(echo $title | cut -d $'_' -f1 ) # replace dashes with space
+		datef=$(date -d ${datef} +'%B %d, %Y')
+		printf '<p><small>%s</small></p>\n' "${datef}" > $tmpfile
+		cat $i >> $tmpfile 
+		sed -i "2s/^/## $titlef\n/" $tmpfile # add title on first line
+		# add dinkus and posted date
+		printf '<br><br><p><center><small><pre>* * *</pre></small></center></p>\n' "${datef}" >> $tmpfile
+		pandoc $tmpfile --from markdown --to html --output $tmpfile.html
+		build_blog_page $path.html $tmpfile.html '../'
+		rm -f $tmpfile $tmpfile.html
+		printf '... Done\n'
+	done
 }
 
 build_blog_archive () {
-	BLOG_DIR="${TARGET_DIR}/blog/"
-	> $BLOG_DIR/.archive
-	POSTS=$(ls -r $BLOG_DIR*.md)
-	LENGTH=${#BLOG_DIR} 
-	LENGTH=$(expr $LENGTH + 1) 
+	> ${BLOG_DIR}/.archive
+	POSTS=$(ls -r ${BLOG_DIR}/*.md)
+	length=${#BLOG_DIR} 
+	length=$(( $length + 2))
 	for i in $POSTS; do
 	    j=${i%.*} # remove .md
-	    j=$(echo "$j" | cut -c ${LENGTH}-)
+	    j=$(echo "$j" | cut -c ${length}-)
 	    k="$j.html"
 		# cut out and format date
 		datef=$(echo $j | cut -d '_' -f1)
-		datef=$(date -d $datef +'%b %d %Y')
+		datef=$(date -d ${datef} +'%b %d %Y')
 		# cut out title
 		title=$(echo $j | cut -d '_' -f2)
 	    title=$(echo "$title" | sed 's/-/\ /g')
 	    #title=$(echo "$title" | sed 's/_/\ \&mdash;\ /g')
 	    # make hyperlink
-	    echo "<a href='blog/$k'>$datef - $title</a><br>" >> $BLOG_DIR/.archive
+	    echo "<a href='blog/$k'>${datef} - $title</a><br>" >> ${BLOG_DIR}/.archive
 	done
 }
 
@@ -197,23 +193,23 @@ build_blog_page () {
 	local BUILD_TARGET=${1}
 	local BUILD_SOURCE=${2}
 	local link_prefix=${3}
-
+	
 	add_header "${BUILD_TARGET}" $link_prefix 
-
+	
 	printf '
 	<div class="column middle">
 	<div class="row">'\
 	>> "${BUILD_TARGET}" 
-
+	
 	cat $BUILD_SOURCE >> "${BUILD_TARGET}"
-
+	
 	printf '
 	</div>
 	</div>\n'\
 	>> "${BUILD_TARGET}"
-
+	
 	add_footer "${BUILD_TARGET}"
-
+	
 	printf "\
 	</body>
 	</html>\
@@ -222,28 +218,28 @@ build_blog_page () {
 
 help () {
 	echo "web.sh $VERSION. Available commands:
-	init destination 
-	build destination"
+	build [destination]"
+}
+
+build () {
+	local root=$1
+	echo "Building in directory ${root}"
+	init
+	source_metadata
+	build_page index.html home_text
+   	build_blog_archive
+   	build_blog_post "${BLOG_DIR}/*.md"
+	build_blog_page ${ROOT}/blog.html ${ROOT}/blog/.archive ''
+	get_size ${ROOT}
+	build_page about.html about_text 
+	get_size ${ROOT}
+	echo "Build completed"
+	echo "Total build size: $SIZE kB"
 }
 
 case $1 in
-	init)
-		init
-		source_metadata
-		exit 1
-		;;
 	build)
-		echo "Building ..."
-		source_metadata
-		build_page index.html home_text
-       	build_blog_archive
-       	build_blog_post "${TARGET_DIR}/blog/*.md"
-		build_blog_page ${TARGET_DIR}/blog.html ${TARGET_DIR}/blog/.archive ''
-		get_size $TARGET_DIR
-		build_page about.html about_text 
-		get_size $TARGET_DIR
-		echo "Build completed."
-		echo "Total build size: $SIZE kB"
+		build ${ROOT}
 		exit 1
 		;;
 	*)
